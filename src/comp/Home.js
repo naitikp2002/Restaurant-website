c2055418c6a04f0510576094601079482875ede9
Naitik Patel <naitik2002patel@gmail.com>
2024-02-29 10:17:35 +0530

    testing

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className='banner'>
       <div className='homedata'>
         <div style={{fontSize:"70px",margin:"15px"}}>Food Corner</div>
-        <div style={{fontSize:"30px",margin:"15px"}}>INDIAN FOOD AT CLICK</div>
+        <div style={{fontSize:"30px",margin:"15px"}}>INDIAN FOOD AT CLICK!</div>
         <ButtonSizes/>
       </div>
     </div>
